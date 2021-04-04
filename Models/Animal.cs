ecd93b60b87b95c20768ccf1f0954459308b559f
Feeniixx <70769391+Feeniixx@users.noreply.github.com>
2021-04-04 21:21:05 +0200

    Cw4

---
+++ Models/Animal.cs
@@ -0,0 +1,16 @@
+﻿namespace CW_4.Controllers
+{
+    public class Animal
+    {
+        public int IdAnimal { get; set; }
+        public string Name { get; set; }
+        public string Discription { get; set; }
+        public string Category { get; set; }
+        public string Area { get; set; }
+
+        public override string ToString()
+        {
+            return $"{IdAnimal} {Name} {Discription} {Category} {Area} \n";
+        }
+    }
+}
